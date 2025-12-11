0468b03f5804b2bb9e5b751e9632cc80fab88504
surajkr9572 <surajsingh43006@gmail.com>
2025-12-11 14:35:03 +0530

    UC5 completed

--- EmployeeManagament/Program.cs
+++ EmployeeManagament/Program.cs
@@ -4,27 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int empAttendence = random.Next(0, 3);
+           
             int empHours = 0;
             int empWages = 0;
             int emp_per_hours = 20;
-
-            switch(empAttendence)
+            int totalWorkingDays = 20;
+            int total_emp_wages = 0;
+            for (int i = 0; i < totalWorkingDays; i++)
             {
-                case 1:
-                    empHours = 4;
-                    break;
-                case 2:
-                    empHours = 8;
-                    break;
-                default:
-                    empHours = 0;
-                    break;
+                Random random = new Random();
+                int empAttendence = random.Next(0, 3);
+                switch (empAttendence)
+                {
+                    case 1:
+                        empHours = 4;
+                        break;
+                    case 2:
+                        empHours = 8;
+                        break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+
+                empWages = empHours * emp_per_hours;
+                total_emp_wages += empWages;
+                Console.WriteLine($"Employee Wages: {empWages}");
             }
-            
-            empWages = empHours * emp_per_hours;
-            Console.WriteLine($"Employee Wages: {empWages}");
+            Console.WriteLine($"Toatl Employee Wages: {total_emp_wages}");
         }
     }
 }
