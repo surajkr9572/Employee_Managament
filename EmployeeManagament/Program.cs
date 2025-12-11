94468b9b9b6ecdc5347639fd2386711a10be155d
surajkr9572 <surajsingh43006@gmail.com>
2025-12-11 12:43:54 +0530

    UC1 Completed

---
+++ EmployeeManagament/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EmployeeManagament
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int empAttendence = random.Next(0, 2);
+            if (empAttendence == 1)
+            {
+                Console.WriteLine("Employee Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Absent");
+            }
+        }
+    }
+}
