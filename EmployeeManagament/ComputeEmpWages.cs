366f00f537f5a5e4fcba8cf2e6efd97dbe603a81
surajkr9572 <surajsingh43006@gmail.com>
2025-12-11 15:21:19 +0530

    UC7 completed

---
+++ EmployeeManagament/ComputeEmpWages.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagament
+{
+    internal class ComputeEmpWages
+    {
+
+        private int empHours;
+        private int empWages;
+        private int total_emp_working_days;
+        private int total_emp_wages;
+        private int total_emp_working_hours;
+
+
+        private int emp_per_hours;
+        private int Max_working_hours;
+        private int Max_emp_working_days;
+        public ComputeEmpWages(int emp_per_hours,int Max_working_hours,int Max_emp_working_days)
+        {
+            this.emp_per_hours = emp_per_hours;
+            this.Max_working_hours = Max_working_hours;
+            this.Max_emp_working_days = Max_emp_working_days;
+        }
+        public void calculateWages()
+        {
+            while (total_emp_working_hours < Max_working_hours && total_emp_working_days < Max_emp_working_days)
+            {
+
+                Random random = new Random();
+                int empAttendence = random.Next(0, 3);
+                switch (empAttendence)
+                {
+                    case 1:
+                        empHours = 4;
+                        break;
+                    case 2:
+                        empHours = 8;
+                        break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+                total_emp_working_days++;
+                total_emp_working_hours += empHours;
+                Console.WriteLine($"Days: {total_emp_working_days} Employee Hours: {total_emp_working_hours} Present {empHours}");
+            }
+            total_emp_wages = total_emp_working_hours * emp_per_hours;
+            Console.WriteLine($"Toatl Employee Wages: {total_emp_wages}");
+        }
+    }
+}
--- EmployeeManagament/Program.cs
+++ EmployeeManagament/Program.cs
@@ -4,39 +4,9 @@
     {
         static void Main(string[] args)
         {
-           
-            int empHours = 0;
-            int empWages = 0;
-            int emp_per_hours = 20;
-            int total_emp_working_days = 0;
-            
-            int total_emp_wages = 0;
-            int Max_working_hours = 100;
-            int Max_emp_working_days = 20;
-            int total_emp_working_hours = 0;
-            while (total_emp_working_hours< Max_working_hours && total_emp_working_days< Max_emp_working_days)
-            {
-                
-                Random random = new Random();
-                int empAttendence = random.Next(0, 3);
-                switch (empAttendence)
-                {
-                    case 1:
-                        empHours = 4;
-                        break;
-                    case 2:
-                        empHours = 8;
-                        break;
-                    default:
-                        empHours = 0;
-                        break;
-                }
-                total_emp_working_days++;
-                total_emp_working_hours += empHours;
-                Console.WriteLine($"Days: {total_emp_working_days} Employee Hours: {total_emp_working_hours} Present {empHours}");
-            }
-            total_emp_wages = total_emp_working_hours * emp_per_hours;
-            Console.WriteLine($"Toatl Employee Wages: {total_emp_wages}");
+           ComputeEmpWages compute_emp_wages=new ComputeEmpWages(20,100,20);
+            compute_emp_wages.calculateWages();
+
         }
     }
 }
